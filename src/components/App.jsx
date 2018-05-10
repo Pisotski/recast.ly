59ce1f15a70d137327b754eff89a1e1e466d1faf
Pisotski <vpisotski@gmail.com>
2018-05-09 21:14:19 -0700

    Wrote functionality for app.js and VideoPlayer.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,39 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideo : {window.exampleVideoData[0]},
+      currentVideoList : {window.exampleVideoData}
+    }
+  }
+  
+  onVideoTitleClick() {
+    this.setState({
+      currentVideo: 
+      currentVideoList: 
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.currentVideoList}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos = {window.exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 window.App = App;
